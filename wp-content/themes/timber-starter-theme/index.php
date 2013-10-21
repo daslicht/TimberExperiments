5ca4e422dcbf73b05886e037948549883851268b
daslicht <daslicht@ansolas.de>
2013-10-21 14:59:57 +0200

    updates

--- wp-content/themes/timber-starter-theme/index.php
+++ wp-content/themes/timber-starter-theme/index.php
@@ -1,13 +1,12 @@
 <?php
-
 /**
  * The main template file
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * E.g., it puts together the home page when no home.php file
  *
- * Methods for WPHelper can be found in the /functions sub-directory
+ * Methods for TimberHelper can be found in the /functions sub-directory
  *
  * @package 	WordPress
  * @subpackage 	Timber
@@ -15,14 +14,20 @@
  */
 
 	if (!class_exists('Timber')){
-		echo 'Timber not activated';
+		echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
 	}
-	
-
-
 	$data = Timber::get_context();
-	$posts = Timber::get_posts('TimberPost');
-	$data['posts'] = $posts;
+	ChromePhp::log($data['menu']);
+
+	$data['posts'] = Timber::get_posts();
+
+	$data['foo'] = 'bar';
+
+	$templates = array('index.twig');
+	
+	if (is_home()){
+		array_unshift($templates, 'home.twig');
+	}
 	Timber::render('index.twig', $data);
 
 
